1c3c71b34c7fa7dd54b548cf4207953ea50af1e3
PB-OBriant <braydenobriant@gmail.com>
2025-11-08 02:19:06 -0600

    feat(epic1): add investment flow, urls, models, and migrations

---
+++ portfolio/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.PortfolioListView.as_view(), name="portfolio-list"),
+
+    path("portfolio/new/", views.PortfolioCreateView.as_view(), name="portfolio-create"),
+    path("portfolio/<int:pk>/", views.PortfolioDetailView.as_view(), name="portfolio-detail"),
+    path("portfolio/<int:pk>/edit/", views.PortfolioUpdateView.as_view(), name="portfolio-edit"),
+    path("portfolio/<int:pk>/delete/", views.PortfolioDeleteView.as_view(), name="portfolio-delete"),
+
+    path("investment/new/", views.InvestmentCreateView.as_view(), name="investment-create"),
+    path("investment/<int:pk>/edit/", views.InvestmentUpdateView.as_view(), name="investment-edit"),
+    path("investment/<int:pk>/delete/", views.InvestmentDeleteView.as_view(), name="investment-delete"),
+]
